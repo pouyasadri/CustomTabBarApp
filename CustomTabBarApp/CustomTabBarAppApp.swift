0c43fac11a62a325fbfdd77b186696ea2a7e9d73
pouyasadri <pouyapaghandeh78@gmail.com>
2024-02-07 13:30:14 +0100

    Initial Commit

---
+++ CustomTabBarApp/CustomTabBarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarAppApp.swift
+//  CustomTabBarApp
+//
+//  Created by Pouya Sadri on 07/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
